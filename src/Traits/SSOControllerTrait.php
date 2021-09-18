0f56c78730f084b2c5a1a65d7d3def0069cb7475
lysice <snowist@126.com>
2021-09-18 17:30:44 +0800

    [fix] controller trait function miss

--- src/Traits/SSOControllerTrait.php
+++ src/Traits/SSOControllerTrait.php
@@ -2,12 +2,34 @@
 
 namespace Lysice\LaravelSSO\Traits;
 
+use Illuminate\Http\Request;
 use Lysice\LaravelSSO\LaravelSSOServer;
 
 trait SSOControllerTrait
 {
+    /**
+     * @param LaravelSSOServer $server
+     * @return string
+     */
     public function userInfoMulti(LaravelSSOServer $server)
     {
         return $server->userInfoMulti();
+    }
+
+    /**
+     * @param Request $request
+     * @param LaravelSSOServer $server
+     *
+     * @return mixed
+     */
+    public function loginMulti(Request $request, LaravelSSOServer $server)
+    {
+        $key = $request->get('key');
+
+        return $server->loginMulti(
+            $request->get($key, null),
+            $request->get('password', null),
+            $request->get('key', null)
+        );
     }
 }
